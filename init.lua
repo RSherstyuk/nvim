8ea14fccfa2c9a8b51b8c3cba75e7f903eadecb0
RSherstyuk <rus.sherstyuk@gmail.com>
2026-01-11 15:43:43 +0300

    ref options

--- init.lua
+++ init.lua
@@ -14,53 +14,16 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Установка leader перед lazy (опционально)
 
--- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    -- import плагинов из lua/plugins/
     { import = "plugins" },
   },
   install = { colorscheme = { "habamax" } },
   checker = { enabled = true },
 })
 
-vim.opt.mouse = "a"
-vim.opt.encoding = "utf-8"
-vim.opt.fileencoding = "utf-8"
-vim.opt.number = true
-vim.opt.cursorline = false
-vim.opt.swapfile = false
-vim.opt.scrolloff = 7
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.fileformat = "unix"
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.termguicolors = true
-vim.opt.relativenumber = true
-vim.opt.clipboard = "unnamedplus"
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
-
-vim.g.clipboard = {
-	name = "win32yank",
-	copy = {
-		["+"] = "/home/russh/.soft/win32yank.exe -i --crlf",
-		["*"] = "/home/russh/.soft/win32yank.exe -i --crlf",
-	},
-	paste = {
-		["+"] = "/home/russh/.soft/win32yank.exe -o --lf",
-		["*"] = "/home/russh/.soft/win32yank.exe -o --lf",
-	},
-	cache_enabled = 0,
-}
+require("config.options")
 
 require("config.neo-tree")
 require("neo-tree.command").execute({ action = "close" })
---
+++ lua/config/options.lua
@@ -0,0 +1,42 @@
+local opt = vim.opt
+local g = vim.g
+
+g.mapleader = " "
+g.maplocalleader = "\\"
+
+opt.mouse = "a"
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
+
+opt.number = true
+opt.relativenumber = true
+opt.cursorline = false
+
+opt.swapfile = false
+opt.fileformat = "unix"
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+opt.smartindent = true
+
+opt.scrolloff = 7
+opt.splitbelow = true
+opt.splitright = true
+opt.termguicolors = true
+opt.clipboard = "unnamedplus"
+
+g.clipboard = {
+  name = "win32yank",
+  copy = {
+    ["+"] = "/home/russh/.soft/win32yank.exe -i --crlf",
+    ["*"] = "/home/russh/.soft/win32yank.exe -i --crlf",
+  },
+  paste = {
+    ["+"] = "/home/russh/.soft/win32yank.exe -o --lf",
+    ["*"] = "/home/russh/.soft/win32yank.exe -o --lf",
+  },
+  cache_enabled = 0,
+}
+
